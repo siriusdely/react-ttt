50f16f20fdb1a3173fdbcb5291b141419c573a45
Sirius Dely <siriusdely@icloud.com>
2018-04-23 21:27:21 +0700

    Converting a Function to a Class (https://reactjs.org/docs/state-and-lifecycle.html#converting-a-function-to-a-class)

--- assets/javascripts/main.react.js
+++ assets/javascripts/main.react.js
@@ -1,10 +1,12 @@
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
