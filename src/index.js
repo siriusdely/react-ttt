90c6c928579963850768579cf8e13436cde21573
Sirius Dely <siriusdely@icloud.com>
2018-07-04 18:06:54 +0700

    yarn add director & classnames; use director for hash-routing; add Footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { Router } from 'director/build/director'
+import ClassNames from 'classnames';
 
 // import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
@@ -7,6 +9,10 @@ import 'todomvc-app-css/index.css';
 // import 'todomvc-common/base.js';
 
 import Model from './model';
+import Utils from './utils';
+
+// var Director = require('director/build/director');
+// var Router = Director.Router;
 
 class TodoItem extends Component {
   render() {
@@ -27,6 +33,48 @@ class TodoItem extends Component {
   }
 }
 
+class Footer extends Component {
+  render() {
+    var clearButton = null;
+    if (this.props.completedCount > 0) {
+      clearButton = (
+        <button className='clear-completed'>
+          Clear completed
+        </button>
+      );
+    }
+
+    var activeTodoWording = Utils.pluralize(this.props.count, 'item');
+    var nowShowing = this.props.nowShowing;
+
+    return (
+      <footer className='footer'>
+        <span className='todo-count'>
+          <strong>{ this.props.count }</strong> { activeTodoWording } left
+        </span>
+        <ul className='filters'>
+          <li><a href='#/'
+                 className={ ClassNames({ selected: nowShowing === ALL_TODOS }) }>
+            All
+          </a></li>
+          <li><a href='#/active'
+                 className={ ClassNames({ selected: nowShowing === ACTIVE_TODOS }) }>
+            Active
+          </a></li>
+          <li><a href='#/completed'
+                 className={ ClassNames({ selected: nowShowing === COMPLETED_TODOS }) }>
+            Completed
+          </a></li>
+        </ul>
+      </footer>
+    );
+  }
+}
+
+const ALL_TODOS = 'ALL_TODOS';
+const ACTIVE_TODOS = 'ACTIVE_TODOS';
+const COMPLETED_TODOS = 'COMPLETED_TODOS';
+
 class TodoApp extends Component {
 
   ENTER_KEY = 13;
@@ -34,11 +82,22 @@ class TodoApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nowShowing: ALL_TODOS,
       editing: null,
       newTodo: ''
     };
-
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    var setState = this.setState;
+    var routes = {
+      '/': setState.bind(this, { nowShowing: ALL_TODOS }),
+      '/active': setState.bind(this, { nowShowing: ACTIVE_TODOS }),
+      '/completed': setState.bind(this, { nowShowing: COMPLETED_TODOS })
+    };
+    var router = Router(routes);
+    router.init('/');
   }
 
   handleChange(e) {
@@ -60,7 +119,6 @@ class TodoApp extends Component {
   }
 
   render() {
-    var main;
     var todos = this.props.model.todos;
     // this.props.model.addTodo('Single Todo Title');
     // console.log(JSON.stringify(todos));
@@ -72,6 +130,7 @@ class TodoApp extends Component {
       );
     }, this);
 
+    var main;
     if (todos.length) {
       main = (
         <section className='main'> 
@@ -81,6 +140,20 @@ class TodoApp extends Component {
             { todoItems }
           </ul>
         </section>
+      );
+    }
+
+    var activeTodoCount = todos.reduce(function(accum, todo) {
+      return todo.completed ? accum : accum + 1;
+    }, 0);
+    var completedCount = todos.length = activeTodoCount;
+
+    var footer;
+    if (activeTodoCount || completedCount) {
+      footer = (
+        <Footer count={ activeTodoCount }
+                completedCount={ completedCount }
+                nowShowing={ this.state.nowShowing } />
       );
     }
 
@@ -97,6 +170,7 @@ class TodoApp extends Component {
                    autoFocus={ true } />
           </header>
           { main }
+          { footer }
         </div>
       </section>
     );
