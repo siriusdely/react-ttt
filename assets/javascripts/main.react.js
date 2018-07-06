d3793c4b5015564cf83033007d133604bcc9f026
Sirius Dely <siriusdely@icloud.com>
2018-07-06 14:25:51 +0700

    few code cleanings

--- assets/javascripts/main.react.js
+++ assets/javascripts/main.react.js
@@ -6,44 +6,40 @@ var List = createReactClass({
     return {data: this.props.data};
   },
   dragStart: function(e) {
-    this.dragged = e.currentTarget;
-    e.dataTransfer.effectAllowed = 'move';
-
+    this.dragged = e.target;
+    e.dataTransfer.effectAllowed = "move";
     // Firefox requires dataTransfer data to be set
     e.dataTransfer.setData("text/html", e.currentTarget);
-  },
-  dragEnd: function(e) {
-
-    this.dragged.style.display = "block";
-    this.dragged.parentNode.removeChild(placeholder);
-
-    // Update data
-    var data = this.state.data;
-    var from = Number(this.dragged.dataset.id);
-    var to = Number(this.over.dataset.id);
-    if(from < to) to--;
-    if(this.nodePlacement == "after") to++;
-    data.splice(to, 0, data.splice(from, 1)[0]);
-    this.setState({data: data});
   },
   dragOver: function(e) {
     e.preventDefault();
     this.dragged.style.display = "none";
-    if(e.target.className == "placeholder") return;
+    if (e.target.className === "placeholder") return;
     this.over = e.target;
     // Inside the dragOver method
     var relY = e.clientY - this.over.offsetTop;
     var height = this.over.offsetHeight / 2;
-    var parent = e.target.parentNode;
-
-    if(relY > height) {
+    // console.log(e.clientY, this.over.offsetTop, relY, this.over.offsetHeight, height)
+    var parent = this.over.parentNode;
+    if (relY > height) {
       this.nodePlacement = "after";
-      parent.insertBefore(placeholder, e.target.nextElementSibling);
-    }
-    else if(relY < height) {
+      parent.insertBefore(placeholder, this.over.nextElementSibling);
+    } else if (relY < height) {
       this.nodePlacement = "before"
-      parent.insertBefore(placeholder, e.target);
+      parent.insertBefore(placeholder, this.over);
     }
+  },
+  dragEnd: function(e) {
+    this.dragged.style.display = "block";
+    this.dragged.parentNode.removeChild(placeholder);
+    // Update data
+    var data = this.state.data;
+    var from = Number(this.dragged.dataset.id);
+    var to = Number(this.over.dataset.id);
+    if (from < to) to--;
+    if (this.nodePlacement === "after") to++;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({data: data});
   },
   render: function() {
     var listItems = this.state.data.map((function(item, i) {
@@ -57,7 +53,6 @@ var List = createReactClass({
         </li>
       );
     }).bind(this));
-
     return <ul onDragOver={this.dragOver}>{listItems}</ul>
   }
 });
