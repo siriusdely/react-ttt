cbe1133f7dcc5c2e32dc87e7c49d6456329470b3
Sirius Dely <siriusdely@icloud.com>
2018-07-21 22:48:38 +0700

    move & unify index.js to App.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider} from 'react-redux';
-import 'todomvc-app-css/index.css';
-/*
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './actions';
-*/
+
 import App from './components/App';
-import store from './store';
-
-// Log the initial state
-console.log(store.getState());
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-);
-
-// Dispatch some actions
-/*
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-*/
-
-// Stop listening to state updates
-unsubscribe();
 
 render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
