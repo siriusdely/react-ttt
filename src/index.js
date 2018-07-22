3d9fb72ceebafcc8dcc1f9d3ca0a2783c92db9c1
Sirius Dely <siriusdely@icloud.com>
2018-07-22 08:11:18 +0700

    add configureStore refactoring

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import 'babel-polyfill';
 
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import configureStore from './configureStore.js';
 import { selectSubreddit, fetchPosts, fetchPostsIfNeeded } from './actions.js';
-import rootReducer from './reducers.js';
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware, // let us dispatch() functions
-    loggerMiddleware, // neat middleware that logs actions
-  )
-);
+const store = configureStore();
 
 store.dispatch(selectSubreddit('reactjs'));
 store
